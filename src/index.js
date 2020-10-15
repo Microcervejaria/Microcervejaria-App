f38b9ec3623cb983ade21eed426c3de68cc05561
Vinícius Cantuária <cantuariavc@gmail.com>
2020-10-15 20:29:21 -0300

    Substitui view por rotas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
@@ -9,19 +7,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Routes />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default function Main() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text>Home Beer</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
