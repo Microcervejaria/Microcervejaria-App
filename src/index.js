6752075aacb77133d0a38baa6eda491d0932e454
Vinícius Cantuária <cantuariavc@gmail.com>
2020-10-20 20:13:49 -0300

    Altera estrutura de rotas

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <>
       <StatusBar
         barStyle="light-content"
         backgroundColor="#FCA311"
-        />
+      />
       <Routes />
-    </NavigationContainer>
+    </>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// import AuthRoute from './auth.routes';
-import AppRoute from './app.routes';
+import Main from '../pages/Main';
+import SignIn from '../pages/SignIn';
+
+const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
-  return <AppRoute />
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#FCA311',
+        },
+        headerTintColor: '#FFFFFF',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+        <Screen name="Main" component={Main} />
+        <Screen name="SignIn" component={SignIn} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
