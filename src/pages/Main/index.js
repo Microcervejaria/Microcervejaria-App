f23d7d971d1b7c860812ec6a8cdd4a768c484f17
Vinícius Cantuária <cantuariavc@gmail.com>
2020-11-03 15:43:02 -0300

    Adiciona navegação do menu para limpeza

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState, createContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity, StyleSheet, Text, View, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 import RecipeCard from '../../components/RecipeCard/index';
 import API from '../../services/api';
+
+import Cleaning from '../../assets/icons/cleaning.svg';
+import Play from '../../assets/icons/play.svg';
+import AddEditRecipe from '../../assets/icons/addEditRecipe.svg';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,6 +24,9 @@ const styles = StyleSheet.create({
 export default function Main() {
   const [data, setData] = useState(0);
   const [loading,setLoading]=useState(true);
+
+  const { navigate } = useNavigation();
+
   async function getResponse() {
     setLoading(true);
     const response = await API.get(`receitas`, { headers: {"Authorization": "cervejaria"}});
@@ -30,6 +41,17 @@ export default function Main() {
     !loading && (
       <View style={styles.container}>
         <RecipeCard data={data[0]} />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <TouchableOpacity>
+            <AddEditRecipe width={60} height={60} style={{ margin: 20 }} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Play width={120} height={120} style={{ margin: 20 }} />
+          </TouchableOpacity>
+          <TouchableOpacity onPressOut={ () => navigate("Limpeza") }>
+            <Cleaning width={60} height={60} style={{ margin: 20 }} />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { NativeScreen } from 'react-native-screens';
 
 import SignIn from '../pages/SignIn';
 import RecipeFormPage from '../pages/RecipeForm';
 import QRCodeReader from '../pages/QRCodeReader';
 import Main from '../pages/Main';
-import { NativeScreen } from 'react-native-screens';
+import Cleaning from '../pages/Cleaning';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -25,7 +26,8 @@ export default function Routes() {
         {/* <Screen name="Sign In" component={SignIn} options={{ headerShown: false }} /> */}
         {/* <Screen name="Leitura do QR Code" component={QRCodeReader} options={{ headerBackTitle: "Voltar" }} /> */}
         {/* <Screen name="Main" component={Main} options={{ headerBackTitle: "Sair" }} /> */}
-        <Screen name="Formulário de Receita" component={RecipeFormPage} />
+        {/* <Screen name="Formulário de Receita" component={RecipeFormPage} /> */}
+        <Screen name="Limpeza" component={Cleaning} options={{ headerBackTitle: "Voltar" }} />
       </Navigator>
     </NavigationContainer>
   );
