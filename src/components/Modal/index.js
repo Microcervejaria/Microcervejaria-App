8459854a6d2aa4da736b6ddad5e04ec28892b5da
Vinícius Cantuária <cantuariavc@gmail.com>
2020-11-04 01:24:53 -0300

    Cria modal para confirmação

---
+++ src/components/Modal/index.js
@@ -0,0 +1,98 @@
+import React, { Component, useEffect, useState } from 'react';
+import { View, Button, Text, TouchableOpacity } from 'react-native';
+import { set } from 'react-native-reanimated';
+
+import Alert from '../../assets/icons/alert.svg';
+import Interrogation from '../../assets/icons/interrogation.svg';
+
+export default function Modal(props) {
+  const [icon, setIcon] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    switch (props.type) {
+      case 'iniciarProcesso':
+        setIcon('Interrogation');
+        setDescription('Deseja iniciar o processo?');
+        break;
+      case 'iniciarLimpeza':
+        setIcon('Interrogation');
+        setDescription('Deseja iniciar a limpeza?');
+        break;
+      case 'finalizarEdicao':
+        setIcon('Interrogation');
+        setDescription('Deseja finalizar a edição?');
+        break;
+      case 'encerrarProcesso':
+        setIcon('Alert');
+        setDescription('Deseja encerrar o processo?');
+        break;
+      default:
+        setIcon('Alert');
+        setDescription('Modal não configurada.');
+        break;
+    }
+  }, []);
+
+  return (
+    <View style={{
+      backgroundColor: '#FCA311',
+      height: '25%',
+      width: '75%',
+      borderRadius: '15px',
+      padding: 15
+      }}
+    >
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+        }}
+      >
+        {icon === 'Interrogation' &&
+        (<Interrogation width={75} height={75} style={{ margin: 10 }} />)}
+        {icon === 'Alert' &&
+        (<Alert width={75} height={75} style={{ margin: 10 }} />)}
+        <Text style={{
+          fontSize: '25px',
+          flex: 1,
+          flexWrap: 'wrap'
+          }}
+        >{description}</Text>
+      </View>
+      <View style={{ alignItems: 'center', padding: 10 }}>
+        <View style={{ flexDirection: 'row'}}>
+          <View style={{
+            backgroundColor: '#14213D',
+            borderRadius: '5px',
+            margin: 10,
+            height: '65%',
+            width: '40%',
+            justifyContent: 'center',
+            }}
+          >
+            <Button
+              title='Não'
+              color='#FFF'
+              onPress={() => {}}
+            />
+          </View>
+          <View style={{
+            backgroundColor: '#EB5757',
+            borderRadius: '5px',
+            margin: 10,
+            height: '65%',
+            width: '40%',
+            justifyContent: 'center',
+          }}>
+            <Button
+              title='Sim'
+              color='#FFF'
+              onPress={() => {}}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
