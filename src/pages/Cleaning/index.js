23c77ef09a02d81b2e6f0020b35dc713687e45d8
Vinícius Cantuária <cantuariavc@gmail.com>
2020-11-06 11:51:59 -0300

    Remove warn

--- src/pages/Cleaning/index.js
+++ src/pages/Cleaning/index.js
@@ -46,7 +46,6 @@ export default function Cleaning() {
   async function getResponse() {
     setLoading(true);
     const response = await API.post(`limpeza`, { headers: { "Authorization": "cervejaria" }});
-    console.warn(response.data);
     setLoading(false);
   }
 
