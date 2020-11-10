e8e55b6dbafe4d03bf1e06996987e0b15c69b8ae
matheus456 <matheus456456@gmail.com>
2020-11-10 14:17:48 -0300

    Adiciona processo de fervura

--- src/components/Processes/styles.js
+++ src/components/Processes/styles.js
@@ -22,7 +22,7 @@ const InformationCardText = styled.Text`
 `;
 
 const WarmView = styled.View`
-  margin: 20px 0px 20px;
+  margin: 30px 0px 30px;
   display: flex;
   justify-content: center;
   align-self: center;
@@ -38,4 +38,55 @@ const WarmLabel = styled.Text`
   color: #FCA311;
 `;
 
-export {ProcessLabel, InformationCardView, InformationCardText, WarmView, WarmLabel};
+const ChartView = styled.View`
+  margin: 10px 0px 10px;
+  justify-content: center;
+  align-self: center;
+  align-items: center;
+`
+
+const CurrentProcessLabel = styled.Text`
+  margin: 10px 0px 10px;
+  font-size: 27px;
+  justify-content: center;
+  align-self: center;
+  color: #FCA311;
+`;
+
+const FinishedProcessLabel = styled.Text`
+  margin: 5px 0px 5px;
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  color: #EB5757;
+`;
+
+const UnFinishedProcessLabel = styled.Text`
+  margin: 5px 0px 5px;
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  color: #E5E5E5;
+`;
+
+const ProcessBoilScrollView = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const PhasesBrewView = styled.View`
+  height: 200px;
+  width: 90%;
+  justify-content: center;
+  align-self: center;
+`
+
+const PhasesBoilView = styled.View`
+  height: 300px;
+  width: 90%;
+  justify-content: center;
+  align-self: center;
+`
+
+export {ProcessLabel, InformationCardView, InformationCardText, WarmView, WarmLabel, ChartView, CurrentProcessLabel, FinishedProcessLabel, UnFinishedProcessLabel, PhasesBrewView, PhasesBoilView, ProcessBoilScrollView};
